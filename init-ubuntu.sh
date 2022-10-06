f80cc85c20f3ea8c454c21bc0b7d7b903231540e
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-10-06 13:21:33 +0700

    init

---
+++ init-ubuntu.sh
@@ -0,0 +1,8 @@
+# copy gitconfig
+sudo cp gitconfig /etc/gitconfig
+
+# copy helix config
+sudo cp helix-config.toml ~/.config/helix/config.toml
+
+# copy neovim config
+sudo cp init.vim ~/.config/nvim/init.vim
